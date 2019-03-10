0150fa52d2af17b5eccf2604c13567d1e2e82990
Harish Narayanan <>
2019-03-10 12:43:18 -0400

    home page - stylings

--- trade/src/app/components/shared/shared.module.ts
+++ trade/src/app/components/shared/shared.module.ts
@@ -1,14 +1,16 @@
 import { InputComponent } from './input/input.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [ InputComponent],
+  declarations: [ InputComponent, HeaderComponent, FooterComponent],
   imports: [
     CommonModule
   ],
   exports: [
-    InputComponent
+    InputComponent, HeaderComponent, FooterComponent
   ]
 })
 export class SharedModule { }
